c59ece7bf747f6679d5f67f6dbe21805f7bb19c5
multTMU <71510449+multTMU@users.noreply.github.com>
2022-02-16 14:40:04 -0500

    Solution

---
+++ DeployFcnl.bat
@@ -0,0 +1,12 @@
+set programDestination=C:\FNCL\Program\
+set programSource=C:\Users\9eo\Documents\SourceCode\FNCL\GUI\fastneutroncollaranalysisgui\GuiFastNeutronCollar\bin\x64\Release\
+copy %programSource%GuiFastNeutronCollar.exe %programDestination%
+copy %programSource%FastNeutronCollar.dll %programDestination%
+copy %programSource%GeometrySampling.dll %programDestination%
+copy %programSource%GlobalHelpersDefaults.dll %programDestination%
+copy %programSource%GuiInterface.dll %programDestination%
+copy %programSource%GuiWidgets.dll %programDestination%
+copy %programSource%MathNet.Numerics.dll %programDestination%
+copy %programSource%NeutronCaptureGammaDetector.dll %programDestination%
+copy %programSource%PoliMiRunner.dll %programDestination%
+copy %programSource%Multiplicity.dll %programDestination%
