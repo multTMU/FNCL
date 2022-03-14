48d697dff8daa33a2399cc2e89a3e662b3edf212
Sean O'Brien <obriense@ornl.gov>
2022-03-14 11:53:20 -0400

    Exe and Zip too big, just dump the necessary files here for now

---
+++ Installer/Simulations/Example/Cf252_Active/runPoliMi_MPPost.bat
@@ -0,0 +1,4 @@
+C:\MCNPpolimi\bin\Win\mcnpx_polimi_v270_v200.exe inp=mcnp.inp out=mcnp.out
+C:\MPPost\bin\MPPost_win64.exe FnclLiquidScintDetector.txt
+C:\MPPost\bin\MPPost_win64.exe He3Mp320Detector.txt
+exit
